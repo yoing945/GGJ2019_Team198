8895d0896f8805bf44e812b9f3567752a791b66a
yotsch723 <369061091@qq.com>
2019-01-20 20:06:58 +0800

    增加地图移动

---
+++ Project/Assets/Scripts/LandCtr.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandCtr : MonoBehaviour
+{
+    private float speed = 2.0f;
+    private const float ScreenHalfWidth = 2.86f;
+    // Use this for initialization	
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (this.transform.position.x <= -ScreenHalfWidth)
+        {
+            this.transform.Translate(Vector3.right * ScreenHalfWidth * 2);
+        }
+    }
+}
