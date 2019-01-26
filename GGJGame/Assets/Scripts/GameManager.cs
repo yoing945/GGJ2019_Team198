6f4e3d11e567edbe4086c43dc31e0026c93b73f4
yoing945 <yoing945@outlook.com>
2019-01-26 21:51:13 +0800

    开场动画

---
+++ GGJGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform startGame;
+    
+    private void Awake()
+    {
+        startGame.gameObject.SetActive(true);
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
