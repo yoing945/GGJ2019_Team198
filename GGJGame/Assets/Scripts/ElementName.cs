d467a70eea219bd391c172a3db62ba28cc34448c
yoing945 <yoing945@outlook.com>
2019-01-26 16:02:04 +0800

    更新合成控制器

--- GGJGame/Assets/Scripts/ElementName.cs
+++ GGJGame/Assets/Scripts/ElementName.cs
@@ -10,8 +10,8 @@ public class ElementName
     public const string Women = "Women";                                //女性
     public const string Health = "Health";                              //健康
     public const string Ill = "Ill";                                    //生病
-    public const string Time = "Time";                                  //一个单位时间
-    public const string Money = "Money";                                //一个单位金钱
+    public const string Time = "Time";                                  //单位时间
+    public const string Money = "Money";                                //单位金钱
 
     //[yl] Combine Element Name
     public const string HealthMan = Health + Man;                       //健康男性
@@ -23,7 +23,50 @@ public class ElementName
     public const string HealthYoungMan = HealthBoy + Time;              //健康男青年
     public const string HealthYoungWomen = HealthGirl + Time;           //健康女青年
 
+    //[yl] Has Result Element Name
 
+}
+
+
+//[yl] 简单起见使用单例
+public class ElementNameMgr
+{
+    private static ElementNameMgr instatnce;
+
+    //constructor
+    private ElementNameMgr()
+    {
+        Init();
+    }
+
+    public static ElementNameMgr getInstance()
+    {
+        if (null == instatnce)
+            instatnce = new ElementNameMgr();
+        return instatnce;
+    }
+
+    public Dictionary<string, string[]> elementsDict;
+    
+    public void Init()
+    {
+        elementsDict = new Dictionary<string, string[]>();
+        elementsDict.Add("Man", new string[] { "男性", ElementName.Man });
+        elementsDict.Add("Women", new string[] { "女性", ElementName.Women });
+        elementsDict.Add("Health", new string[] { "健康", ElementName.Health });
+        elementsDict.Add("Ill", new string[] { "生病", ElementName.Ill });
+        elementsDict.Add("Time", new string[] { "单位时间", ElementName.Time });
+        elementsDict.Add("Money", new string[] { "单位金钱", ElementName.Money });
+        elementsDict.Add("HealthMan", new string[] { "健康男性", ElementName.HealthMan });
+        elementsDict.Add("HealthMan", new string[] { "健康女性", ElementName.HealthMan });
+        elementsDict.Add("IllMan", new string[] { "生病男性", ElementName.IllMan });
+        elementsDict.Add("IllWomen", new string[] { "生病女性", ElementName.IllWomen });
+        elementsDict.Add("HealthBoy", new string[] { "健康男孩", ElementName.HealthBoy });
+        elementsDict.Add("HealthGirl", new string[] { "健康女孩", ElementName.HealthGirl });
+        elementsDict.Add("HealthYoungMan", new string[] { "健康男青年", ElementName.HealthYoungMan });
+        elementsDict.Add("HealthYoungWomen", new string[] { "健康女青年", ElementName.HealthYoungWomen });
+
+    }
 }
 
 
--- GGJGame/Assets/Scripts/HandleElement/CombineController.cs
+++ GGJGame/Assets/Scripts/HandleElement/CombineController.cs
@@ -3,18 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //[yl] 元素组合控制器
-public class CombineController : MonoBehaviour
+public class CombineController
 {
 
-    public Dictionary<string[], string> CombineTable;
-
-    public void InitCombineTable()
+    //[yl] 组合,返回组合而成的元素名英文名
+    public static string DoCombine(string[] elementNames)
     {
-        
-    }
+        var elementDict = ElementNameMgr.getInstance().elementsDict;
+        foreach( var key in elementDict.Keys)
+        {
+            string elementFullNameJudge = CustomFunc.DeepCopy<string>(elementDict[key][1]);
+            for(int i = 0; i < elementNames.Length; ++i)
+            {
+                var curEEnName = elementNames[i];
+                if (!elementDict.ContainsKey(curEEnName))
+                {
+                    Debug.LogError("No such Key:" + curEEnName + " In elementDict");
+                    return "";
+                }
 
-    //[yl] 组合
-    public void DoCombine()
-    {
+                var curEFullName = elementDict[curEEnName][1];
+                //如果不包含元素全名,那么目标元素无法组合
+                if(!elementFullNameJudge.Contains(curEFullName))
+                {
+                    break;
+                }
+                elementFullNameJudge = elementFullNameJudge.Replace(curEFullName, "");
+                if(elementFullNameJudge == "")
+                {
+                    if(i == elementNames.Length - 1)
+                    {
+                        return key;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+        return "";
     }
 }
