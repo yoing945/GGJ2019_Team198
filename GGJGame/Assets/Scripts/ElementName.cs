cc4d0d1746cd2ab5a2adbc83ca8bc5798d2cb24a
yoing945 <yoing945@outlook.com>
2019-01-26 13:39:41 +0800

    增加combine操作脚本

---
+++ GGJGame/Assets/Scripts/ElementName.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[yl] 元素名
+public class ElementName
+{
+    //[yl] Base Element Name
+    public const string Man = "Man";            //男人
+    public const string Women = "Women";        //女人
+    public const string Health = "Health";      //健康
+    public const string Ill = "Ill";            //生病
+    public const string Time = "Time";          //一个单位时间
+    public const string Money = "Money";        //一个单位金钱
+
+    //[yl] Combine Element Name
+    public const string HealthMan = Health + Man;
+    public const string HealthWomen = Health + Women;
+    public const string IllMan = Ill + Man;
+    public const string IllWomen = Ill + Women;
+    public const string HealthBoy = HealthMan + Time;
+    public const string HealthGirl = HealthWomen + Time;
+    public const string HealthYoungMan = HealthBoy + Time;
+    public const string HealthYoungWomen = HealthGirl + Time;
+
+
+}
+
+
---
+++ GGJGame/Assets/Scripts/HandleElement/CombineController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[yl] 元素组合控制器
+public class CombineController : MonoBehaviour
+{
+
+    public Dictionary<string[], string> CombineTable;
+
+    public void InitCombineTable()
+    {
+        
+    }
+
+    //[yl] 组合
+    public void DoCombine()
+    {
+    }
+}
