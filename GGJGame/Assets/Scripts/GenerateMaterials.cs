f9c10d310e3b14ba502b076af43d1e0feef8e338
Simon <576316262@qq.com>
2019-01-26 15:08:47 +0800

    prefab

--- GGJGame/Assets/Scripts/GenerateMaterials.cs
+++ GGJGame/Assets/Scripts/GenerateMaterials.cs
@@ -15,8 +15,12 @@ public class GenerateMaterials : MonoBehaviour
     public Transform root_left;
     public Transform root_right;
     public GameObject prefab_mat;
+    public Transform root_drag;
 
     public List<MaterialData> matList = new List<MaterialData>();
+
+    private bool isDrag = false;
+    private GameObject target = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,7 +31,16 @@ public class GenerateMaterials : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            isDrag = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDrag = false;
+        }
+
+        RayMove();
     }
 
     private void GenerateMats()
@@ -48,5 +61,34 @@ public class GenerateMaterials : MonoBehaviour
                 mat.transform.SetParent(root_right);
             }
         }
+    }
+
+    private void RayMove()
+    {
+        
+        if (!isDrag) return;
+       
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (isDrag)
+        {
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("AAAAAAA");
+                if (target == null)
+                {
+                    target = Instantiate(prefab_mat);
+                    target.transform.SetParent(root_drag);
+                    target.transform.position = transform.position;
+                }
+                Vector3 offset = Input.mousePosition;
+                target.transform.position = new Vector3(hit.point.x, hit.point.y, hit.transform.position.z);
+                Debug.DrawLine(ray.origin, hit.point);
+            }
+        }
+
+
     }
 }
