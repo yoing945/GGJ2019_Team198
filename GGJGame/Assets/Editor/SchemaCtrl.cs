a7d457cd647b2d88952c0178cbb00211b520f8b3
yotsch723 <369061091@qq.com>
2019-01-26 21:42:20 +0800

    update 增加数据结构

---
+++ GGJGame/Assets/Editor/SchemaCtrl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SchemaCtrl 
+{
+    [MenuItem("Assets/CreateEditor/SchemaPos")]
+    public static Res CreatePosSchema()
+    {
+        Res asset = ScriptableObject.CreateInstance<Res>();
+        AssetDatabase.CreateAsset(asset, "Assets/Resources/PosSchema.asset");
+        AssetDatabase.SaveAssets();
+        return asset;
+    }
+  
+}
---
+++ GGJGame/Assets/Scripts/PhotoPos.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotoPos 
+{
+    public class ContainerPos : ScriptableObject
+    {
+        public List<Vector2> photoPos = new List<Vector2>();
+    }
+    public class ContainerSprite : ScriptableObject
+    {
+        public List<Sprite> sprites = new List<Sprite>();
+    }
+}
