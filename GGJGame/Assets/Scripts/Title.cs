d530c3129fdd4c9fd27ab57f67cd9d862e94e240
yotsch723 <369061091@qq.com>
2019-01-26 15:51:44 +0800

    update Resources/font/字体管家方萌.ttf.meta

---
+++ GGJGame/Assets/Scripts/Title.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Title : MonoBehaviour
+{
+    public Text Quest;
+    public Text Answer;
+    public GameObject Block;
+    // Start is called before the first frame update
+    public void SetQuest(string Content)
+    {
+        Quest.text = Content;
+        Block.SetActive(false);
+        Quest.color = new Color(Quest.color.r, Quest.color.g, Quest.color.b, 1.0f);
+    }
+    public void SetAnswer(string Content)
+    {
+        if(Quest.text == Content)
+        {
+            Block.SetActive(false);
+            //正确答案的效果在这里
+        }
+        else
+        {
+            Incorrect();
+        }
+    }
+    private void Incorrect()
+    {
+        Quest.color = new Color(Quest.color.r, Quest.color.g, Quest.color.b, 0.2f);
+        Answer.gameObject.SetActive(true);
+        Block.SetActive(true);
+    }
+}
