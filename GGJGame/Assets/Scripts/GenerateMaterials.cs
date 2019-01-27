c90d8017733f1797d4afdb4d88560915d86fac53
Simon <576316262@qq.com>
2019-01-27 12:44:43 +0800

    人物顺序，以及清空照片

--- GGJGame/Assets/Scripts/GenerateMaterials.cs
+++ GGJGame/Assets/Scripts/GenerateMaterials.cs
@@ -37,6 +37,9 @@ public class GenerateMaterials : MonoBehaviour
     public Transform root_char;
     public GameObject prefab_char;
     private List<Vector2> photo_char_pos = new List<Vector2>();
+    //按生成顺序存放的list，需要清空
+    private List<Sprite> tempOrderSprList = new List<Sprite>();
+    private List<Vector2> tempOrderVec2List = new List<Vector2>();
 
     void Start()
     {
@@ -97,8 +100,6 @@ public class GenerateMaterials : MonoBehaviour
             Destroy(root_drag.GetChild(i).gameObject);
         }
 
-        //照片还是新素材？
-
         //播放闪光动画
         if (flash != null)
         {
@@ -115,14 +116,25 @@ public class GenerateMaterials : MonoBehaviour
             var photoSprites = CombineController.getResultSprites(str_arr);
             if (photoSprites != null)
             {
+                ClearPhoto();
                 if (photoSprites.Count > 0 && photo_bg != null)
                     photo_bg.sprite = photoSprites[0];
                 if (photoSprites.Count > 1 && prefab_char != null)
                 {
                     //每次生成前打乱固定位置
                     MessPosOrder();
-                    for (int i = 1; i < photoSprites.Count; i++)
-                        GenerateChar(photoSprites[i], i);
+                    
+                    if (photoSprites.Count - 1 < photo_char_pos.Count)
+                    {
+                        for (int i = 1; i < photoSprites.Count; i++)
+                        {
+                            tempOrderSprList.Add(photoSprites[i]);
+                            tempOrderVec2List.Add(photo_char_pos[i - 1]);
+                        }
+                        GetGenerateOrder();
+                    }
+                    for (int i = 0; i < tempOrderVec2List.Count; i++)
+                        GenerateChar(tempOrderSprList[i], tempOrderVec2List[i]);
                 }
             }
 
@@ -147,15 +159,14 @@ public class GenerateMaterials : MonoBehaviour
         }
     }
 
-    private GameObject GenerateChar(Sprite spr, int index)
+    private GameObject GenerateChar(Sprite spr, Vector2 pos)
     {
         GameObject character = Instantiate(prefab_char);
         character.GetComponent<Image>().sprite = spr;
         character.transform.SetParent(root_char);
         var rt = character.GetComponent<RectTransform>();
         rt.localScale = Vector3.one;
-        if (photo_char_pos.Count > index)
-            rt.anchoredPosition = photo_char_pos[index];
+        rt.anchoredPosition = pos;
         return character;
     }
 
@@ -169,5 +180,46 @@ public class GenerateMaterials : MonoBehaviour
             photo_char_pos.RemoveAt(rand);
             photo_char_pos.Add(item);
         }
+    }
+
+    private void GetGenerateOrder()
+    {
+        //UI根据Y值从大到小的顺序
+        int sum = tempOrderVec2List.Count;
+        for (int i = 0; i < sum; i++)
+        {
+            int maxIndex = sum - 1 - i;
+            var maxItem = tempOrderVec2List[maxIndex];
+            for (int j = maxIndex - 1; j >= 0; j--)
+            {
+                if (tempOrderVec2List[j].y > maxItem.y)
+                {
+                    maxItem = tempOrderVec2List[j];
+                    maxIndex = j;
+                }
+            }
+            tempOrderVec2List.RemoveAt(maxIndex);
+            tempOrderVec2List.Add(maxItem);
+            //同步sprite顺序
+            var item = tempOrderSprList[maxIndex];
+            tempOrderSprList.RemoveAt(maxIndex);
+            tempOrderSprList.Add(item);
+        }
+    }
+
+    public void ClearPhoto()
+    {
+
+        if(root_char != null)
+        {
+            for (int i = 0; i < root_char.childCount; i++)
+                Destroy(root_char.GetChild(i).gameObject);
+        }
+        if (photo_bg != null)
+            photo_bg.sprite = null;
+
+        //每次生成人物前清空
+        tempOrderSprList.Clear();
+        tempOrderVec2List.Clear();
     }
 }
